57896e2c73c8e27dc03e8fab473b5bf5ab819f73
Julien <arzourjulien@gmail.com>
2024-02-07 12:21:33 +0100

    Modification du bouton + creat

--- nextjs-tpnote/src/app/dashboard/page.tsx
+++ nextjs-tpnote/src/app/dashboard/page.tsx
@@ -1,9 +1,13 @@
+import { PlusIcon } from '@heroicons/react/24/solid';
+
 export default function Page() {
   return (
     <div className="flex h-screen">
       {/* Partie gauche - occupe 2/3 de la largeur */}
       <div className="w-2/3 h-full">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+ Create</button>
+        <button className="bg-purple-600 hover:bg-purple-700 text-white font-bold text-sm py-1 px-3 rounded-full flex items-center">
+          <PlusIcon className="h-5 w-5 mr-1" /> Create
+        </button>
         {/* Le reste du contenu de la page de tableau de bord */}
       </div>
       {/* Partie droite - occupe 1/3 de la largeur */}
