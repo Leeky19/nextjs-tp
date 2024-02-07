15f4fb58de1de7de654d550b698c598bfd95687f
Julien <arzourjulien@gmail.com>
2024-02-07 12:07:51 +0100

    ajout du bouton + creat et séparation en deux partie de la page du tableau

--- nextjs-tpnote/src/app/dashboard/page.tsx
+++ nextjs-tpnote/src/app/dashboard/page.tsx
@@ -1,3 +1,15 @@
 export default function Page() {
-    return <p>Dashboard Page</p>;
-  }
+  return (
+    <div className="flex h-screen">
+      {/* Partie gauche - occupe 2/3 de la largeur */}
+      <div className="w-2/3 h-full">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">+ Create</button>
+        {/* Le reste du contenu de la page de tableau de bord */}
+      </div>
+      {/* Partie droite - occupe 1/3 de la largeur */}
+      <div className="w-1/3 h-full bg-yellow-800">
+        {/* Contenu de la partie droite */}
+      </div>
+    </div>
+  );
+}
