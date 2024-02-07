f32dc1acfa52d546631fe90c4f6eb97df7085762
Julien <arzourjulien@gmail.com>
2024-02-07 10:56:04 +0100

    modification des couleurs de la sidebar

--- nextjs-tpnote/src/app/ui/dashboard/nav-links.tsx
+++ nextjs-tpnote/src/app/ui/dashboard/nav-links.tsx
@@ -35,9 +35,9 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-purple-950 p-3 text-sm text-purple-100 font-medium hover:bg-purple-300 hover:text-purple-500 md:flex-none md:justify-start md:p-2 md:px-3',
                 {
-                  'bg-sky-100 text-blue-600': pathname === link.href,
+                  'bg-purple-300 text-purple-500': pathname === link.href,
                 },
               )}
             >
